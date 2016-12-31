6105045d1f336b859e8ee09d5f5a18d939be0fac
Sarah Lawrence <s.j.lawrence8@gmail.com>
2016-12-31 22:15:11 +1100

    add var's and remove comments

--- LED_blinks/LED/LED.ino
+++ LED_blinks/LED/LED.ino
@@ -1,33 +1,40 @@
-int redPin = 3;
-int yellowPin = 6;
+const int ledAlpha = 3; // currently red
+const int ledBeta = 6; // currently blue
+const int slowInterval = 500;
+const int midInterval = 250;
+const int fastInterval = 100;
 
-// the setup function runs once when you press reset or power the board
+
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(redPin, OUTPUT);
+  pinMode(ledAlpha, OUTPUT);
+  pinMode(ledBeta, OUTPUT);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(redPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(yellowPin, LOW);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);    // turn the LED off by making the voltage LOW
-  digitalWrite(redPin, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(yellowPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(redPin, HIGH);    // turn the LED off by making the voltage LOW
-  digitalWrite(yellowPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);    // turn the LED off by making the voltage LOW
-  digitalWrite(redPin, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(yellowPin, LOW);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(redPin, LOW);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(yellowPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(ledAlpha, HIGH);
+  digitalWrite(ledBeta, LOW);
+  delay(100);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(ledAlpha, LOW);
+  digitalWrite(ledBeta, HIGH);
+  delay(100);
+
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(ledAlpha, HIGH);
+  digitalWrite(ledBeta, HIGH);
+  delay(100);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(ledAlpha, LOW);
+  digitalWrite(ledBeta, LOW);
+  delay(100);
+  
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(ledAlpha, LOW);
+  digitalWrite(ledBeta, HIGH);
   delay(100);
 }
