ff310fde81b19fec840ccbf1fe21f434d9ac6554
Sarah Lawrence <s.j.lawrence8@gmail.com>
2017-01-01 12:10:36 +1100

    Added random light and delay; fixed compiling errors

--- LED_blinks/LED/LED.ino
+++ LED_blinks/LED/LED.ino
@@ -1,40 +1,48 @@
-const int ledAlpha = 3; // currently red
-const int ledBeta = 6; // currently blue
+const int ledAlpha = 13;
+const int ledBravo = 3;
+const int ledCharlie = 6;
 
 const int slowInterval = 500;
 const int midInterval = 250;
 const int fastInterval = 100;
 
-
+int ledArray[3] = {ledAlpha, ledBravo, ledCharlie};
+int delayArray[3] = {slowInterval, midInterval, fastInterval};
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(ledAlpha, OUTPUT);
-  pinMode(ledBeta, OUTPUT);
+  pinMode(ledBravo, OUTPUT);
+  pinMode(ledCharlie, OUTPUT);
 }
 
 void loop() {
-  // turn light on then delay before next light
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(midInterval);
 
-  digitalWrite(ledAlpha, HIGH);
-  delay(fastInterval);
+  // choose an LED at random
+  int randomLED = rand() % 3;
+  int selectedLED = ledArray[randomLED];
 
-  digitalWrite(ledBeta, HIGH);
-  delay(slowInterval);
+  // choose a random delay amount
+  int randomDelay = rand() % 3;
+  int selectedDelay = delayArray[randomLED];
+  
+
+  // turn on selected LED for selected delay
+  digitalWrite(selectedLED, HIGH);
+  delay(selectedDelay);
+
+
 
   // turn all lights off at end of loop
-  if (digitalRead(LED_BUILTIN, HIGH)) {
+  if (digitalRead(LED_BUILTIN) == HIGH) {
     digitalWrite(LED_BUILTIN, LOW);
   }
 
-  if (digitalRead(ledAlpha, HIGH)) {
+  if (digitalRead(ledAlpha) == HIGH) {
     digitalWrite(ledAlpha, LOW);
   }
 
-  if (digitalRead(ledBeta, HIGH)) {
-    digitalWrite(ledBeta, LOW);
+  if (digitalRead(ledBravo) == HIGH) {
+    digitalWrite(ledBravo, LOW);
   }
 
 }
