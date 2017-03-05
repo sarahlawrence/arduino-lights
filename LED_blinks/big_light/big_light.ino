8de927bd490dce4b5ad5879aee6fa980c4b7bf0f
Sarah Lawrence <s.j.lawrence8@gmail.com>
2017-03-05 15:09:43 +1100

    Pos initialised, pos 1 lights up

---
+++ LED_blinks/big_light/big_light.ino
@@ -0,0 +1,24 @@
+const int pos1 = 2;
+const int pos2 = 3;
+const int pos4 = 4;
+const int pos5 = 5;
+const int pos6 = 6;
+const int pos7 = 7;
+const int pos9 = 8;
+const int pos10 = 9;
+
+void setup() {
+  pinMode(pos1, OUTPUT);
+  pinMode(pos2, OUTPUT);
+  pinMode(pos4, OUTPUT);
+  pinMode(pos5, OUTPUT);
+  pinMode(pos6, OUTPUT);
+  pinMode(pos7, OUTPUT);
+  pinMode(pos9, OUTPUT);
+  pinMode(pos10, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(pos1, HIGH);
+
+}
